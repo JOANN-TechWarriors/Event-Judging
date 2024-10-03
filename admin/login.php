9728bbbae91ccebb381f850c053a822e3ec648f4
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-03 23:07:29 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../images/logo copy.png"/>
     <title>Event Judging System</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
@@ -11,6 +12,7 @@
 
 include('dbcon.php');
 session_start();
+
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
@@ -25,7 +27,22 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $num_row = $query->rowCount();
 
     if ($num_row > 0) { 
-        if ($row['access'] == "Organizer") {
+        if ($row['request_status'] == '') {
+            ?>
+            <script>
+                Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Sorry, Your account is not yet approve by the admin',
+                        confirmButtonText: 'OK'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location = 'index.php';
+                        }
+                    });
+            </script>
+            <?php
+        }elseif ($row['access'] == "Organizer") {
             $_SESSION['useraccess'] = "Organizer";
             $_SESSION['id'] = $row['organizer_id'];
             ?>
