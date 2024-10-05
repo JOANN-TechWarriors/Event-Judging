b2998c3f620de36945d61ec9042ece58414854c7
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-05 16:01:58 +0800

    commit

--- admin/judge_profile.php
+++ admin/judge_profile.php
@@ -1,6 +1,7 @@
 <?php 
 include('session.php');
 
+
 $judge_code=$_POST['judge_code'];
 
 	$query = $conn->query("SELECT * FROM judges WHERE code='$judge_code'");
@@ -18,18 +19,29 @@ $subevent_id=$row['subevent_id'];
 
 ?>
 
-            <script>
-            
-            window.location.href = "judge_panel.php?judge_ctr=<?php echo $judge_ctr; ?>&subevent_id=<?php echo $subevent_id; ?>";
-            </script>
-<?php }
-else
-{ ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
 
 <script>
-alert('wrong code');
- 			
-window.location = 'welcome.php';
+<?php if (/* your condition here */) { ?>
+	swal({
+		title: "Success!",
+		text: "Redirecting to judge panel...",
+		type: "success",
+		timer: 2000,
+		showConfirmButton: false
+	}, function(){
+		window.location.href = "judge_panel.php?judge_ctr=<?php echo $judge_ctr; ?>&subevent_id=<?php echo $subevent_id; ?>";
+	});
+<?php } else { ?>
+	swal({
+		title: "Error!",
+		text: "Wrong code",
+		type: "error",
+		confirmButtonText: "OK"
+	}, function(){
+		window.location = 'welcome.php';
+	});
+<?php } ?>
 </script>
     
 <?php }
