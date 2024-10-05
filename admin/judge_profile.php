d8b6325876d9517f99f116feb3132cefd520d5e6
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-05 16:12:58 +0800

    commit

--- admin/judge_profile.php
+++ admin/judge_profile.php
@@ -1,48 +1,41 @@
 <?php 
 include('session.php');
 
-$judge_code=$_POST['judge_code'];
+$judge_code = $_POST['judge_code'];
 
-	$query = $conn->query("SELECT * FROM judges WHERE code='$judge_code'");
-			$row = $query->fetch();
-			$num_row = $query->rowcount();
-            
-            
-            
-            
-		if( $num_row > 0 ) { 
-		  
-          
-$judge_ctr=$row['judge_ctr'];
-$subevent_id=$row['subevent_id'];
+$query = $conn->query("SELECT * FROM judges WHERE code='$judge_code'");
+$row = $query->fetch();
+$num_row = $query->rowcount();
 
+if ($num_row > 0) {
+    $judge_ctr = $row['judge_ctr'];
+    $subevent_id = $row['subevent_id'];
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Correct code. Redirecting...',
+            timer: 2000,
+            showConfirmButton: false
+        }).then(() => {
+            window.location.href = "judge_panel.php?judge_ctr=<?php echo $judge_ctr; ?>&subevent_id=<?php echo $subevent_id; ?>";
+        });
+    </script>
+<?php
+} else {
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Wrong code. Please try again.',
+        }).then(() => {
+            window.location.href = 'welcome.php';
+        });
+    </script>
+<?php
+}
 ?>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-
-<script>
-swal({
-	title: "Success!",
-	text: "Redirecting to judge panel...",
-	type: "success",
-	timer: 2000,
-	showConfirmButton: false
-}, function(){
-	window.location.href = "judge_panel.php?judge_ctr=<?php echo $judge_ctr; ?>&subevent_id=<?php echo $subevent_id; ?>";
-});
-</script>
-<?php }
-else
-{ ?>
-
-<script>
-alert('wrong code');
- 			
-window.location = 'welcome.php';
-</script>
-    
-<?php }
-?>
-
-
-                                                
