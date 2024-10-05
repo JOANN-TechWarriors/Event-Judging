2c3612cd3a5c1dd0f31bb7f368c397e89bef594e
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-05 16:34:08 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -67,6 +67,22 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         </script>
         <?php
     }
+} else {
     ?>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Invalid User or Password',
+            confirmButtonText: 'OK'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location = 'index.php';
+            }
+        });
+    </script>
+    <?php
+}
+?>
 </body>
 </html>
