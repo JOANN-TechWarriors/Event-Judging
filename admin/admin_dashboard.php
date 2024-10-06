e9e19fa18bf17c6ff56c85b92e6add6af13dbc3e
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-06 17:55:14 +0800

    Update admin_dashboard.php

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,5 +1,17 @@
 <?php 
 session_start();
+$host = '127.0.0.1';
+$username = 'u510162695_judging_root';
+$password = '1Judging_root';  // Replace with the actual password
+$dbname = 'u510162695_judging';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
 if (!isset($_SESSION['admin_id'])) {
    header('location: admin_login.php');
 }
@@ -258,7 +270,6 @@ if (!isset($_SESSION['admin_id'])) {
                     <div class="card-body">
                         <h4 class="font-weight-normal mb-3" style="font-size: 20px;">Ongoing Events</h4>
                         <?php 
-                        $database = mysqli_connect('localhost', 'root', '', 'judging');
                         $sql = "SELECT count(1) FROM sub_event";
                         $result = mysqli_query($database, $sql);
                         $row = mysqli_fetch_array($result);
