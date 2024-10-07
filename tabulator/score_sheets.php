b29405f318c8f7c5dbe15a1b762a5d1f8f8aaff9
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-07 15:38:23 +0800

    commit

--- tabulator/score_sheets.php
+++ tabulator/score_sheets.php
@@ -240,6 +240,7 @@ include('..//admin/session.php');
             <div>Event Judging System</div>
         </div>
         <ul>
+
             <li><a href="score_sheets.php"><i class="fas fa-clipboard-list"></i> <span>SCORE SHEETS</span></a></li>
             <li><a href="rev_main_event.php"><i class="fas fa-chart-line"></i> <span>DATA REVIEWS</span></a></li>
         </ul>
