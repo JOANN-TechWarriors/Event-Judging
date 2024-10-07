98e6d8f3dfb7d60f2558337a9f68336a5e77ccad
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-07 16:59:59 +0800

    commit

--- admin/home.php
+++ admin/home.php
@@ -232,7 +232,7 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     .tile {
-        background-color: linear-gradient(to bottom, #33ccff 0%, #ff99cc 100%;
+        background-color: #99FFFF;
         padding: 20px;
         border-radius: 10px;
         text-align: center;
