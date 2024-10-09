753ee63603d4d93a841f9fad9039c0465b19b90d
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-09 16:08:45 +0800

    commit

--- admin/review_search.php
+++ admin/review_search.php
@@ -16,90 +16,89 @@ $txtsearch = $_POST['txtsearch'];
     }
 
     .sidebar {
-      position: fixed;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 250px;
-      background-color: #333;
-      color: #fff;
-      padding-top: 20px;
-      transition: all 0.3s;
-      overflow: hidden;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 250px;
+        background-color: #27293d;
+        color: #fff;
+        padding-top: 20px;
+        transition: all 0.3s;
+        overflow: hidden;
     }
 
     .sidebar.collapsed {
-      width: 80px;
+        width: 80px;
     }
 
     .sidebar .toggle-btn {
-      position: absolute;
-      top: 15px;
-      right: -0px;
-      background-color: transparent;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-      transition: all 0.3s;
+        position: absolute;
+        top: 10px;
+        right: 18px;
+        background-color: transparent;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        transition: all 0.3s;
     }
 
     .sidebar .toggle-btn i {
-      font-size: 18px;
+        font-size: 20px;
     }
 
     .sidebar-heading {
-      text-align: center;
-      padding: 10px 0;
-      font-size: 18px;
-      margin-bottom: 10px;
+        text-align: center;
+        padding: 10px 0;
+        font-size: 18px;
+        margin-bottom: 10px;
     }
 
     .sidebar-heading img {
-      max-width: 100px;
-      max-height: 100px;
+        max-width: 100px;
+        max-height: 100px;
     }
 
     .sidebar ul {
-      list-style-type: none;
-      padding: 0;
-      margin: 0;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
     }
 
     .sidebar ul li {
-      padding: 10px;
-      border-bottom: 1px solid #555;
-      transition: all 0.3s;
+        padding: 15px 20px;
+        transition: all 0.3s;
     }
 
     .sidebar ul li a {
-      color: #fff;
-      text-decoration: none;
-      display: flex;
-      align-items: center;
+        color: #fff;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
     }
 
     .sidebar ul li a i {
-      margin-right: 10px;
-      transition: margin 0.3s;
+        margin-right: 10px;
+        transition: margin 0.3s;
     }
 
     .sidebar.collapsed ul li a i {
-      margin-right: 0;
+        margin-right: 0;
     }
 
     .sidebar ul li a span {
-      display: inline-block;
-      transition: opacity 0.3s;
+        display: inline-block;
+        transition: opacity 0.3s;
     }
 
     .sidebar.collapsed ul li a span {
-      opacity: 0;
-      width: 0;
-      overflow: hidden;
+        opacity: 0;
+        width: 0;
+        overflow: hidden;
     }
 
     .sidebar ul li a:hover {
-      background-color: #555;
+        background-color: #1a1a2e;
     }
 
     .main {
@@ -133,24 +132,18 @@ $txtsearch = $_POST['txtsearch'];
       <div>Event Judging System</div>
     </div>
     <ul>
-      <li><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i> <span>DASHBOARD</span></a></li>
-      <li><a href="home.php"><i class="fas fa-calendar-check"></i> <span>ONGOING EVENTS</span></a></li>
-      <li><a href="upcoming_events.php"><i class="fas fa-calendar-alt"></i> <span>UPCOMING EVENTS</span></a></li>
-      <li><a href="score_sheets.php"><i class="fas fa-clipboard-list"></i> <span>SCORE SHEETS</span></a></li>
+      <li><a href="../tabulator/score_sheets.php"><i class="fas fa-clipboard-list"></i> <span>SCORE SHEETS</span></a></li>
       <li><a href="rev_main_event.php"><i class="fas fa-chart-line"></i> <span>DATA REVIEWS</span></a></li>
-      <li><a href="#" id="logout"><i class="fas fa-sign-out-alt"></i> <span>LOGOUT</span></a></li>
     </ul>
   </div>
   <div class="main" id="main-content">
     <div class="container">
-      <h1>Data Reviews</h1>
+      <h1 style="font-size: 35px;">Data Reviews</h1>
     </div>
     <div class="container">
       <br />
       <div class="col-md-12">
         <ul class="breadcrumb">
-          <li><a href="dashboard.php">Dashboard</a> / </li>
-          <li><a href="home.php">Ongoing Events</a> / </li>
           <li><a href="rev_main_event.php">Data Reviews</a> / </li>
           <li>Data Reviews: Search</li>
         </ul>
