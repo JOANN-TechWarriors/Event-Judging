8bd46baed69ba8402ed0d2476a0fd9dff7e6d627
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-09 15:47:31 +0800

    commit

--- admin/sub_event_details_edit.php
+++ admin/sub_event_details_edit.php
@@ -191,8 +191,8 @@ $se_row = $se_query->fetch();
       <li><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i> <span>DASHBOARD</span></a></li>
       <li><a href="home.php"><i class="fas fa-calendar-check"></i> <span>ONGOING EVENTS</span></a></li>
       <li><a href="upcoming_events.php"><i class="fas fa-calendar-alt"></i> <span>UPCOMING EVENTS</span></a></li>
-      <li><a href="score_sheets.php"><i class="fas fa-clipboard-list"></i> <span>SCORE SHEETS</span></a></li>
-      <li><a href="rev_main_event.php"><i class="fas fa-chart-line"></i> <span>DATA REVIEWS</span></a></li>
+      <li><a href="live.php"><i class="fas fa-camera"></i> <span>LIVE</span></a></li>
+
     </ul>
   </div>
 
