779bffe0a4bcdfd9b67ef583e55217fa41a0856e
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-09 16:14:36 +0800

    commit

--- admin/rev_sub_event.php
+++ admin/rev_sub_event.php
@@ -147,7 +147,7 @@
   <!-- Subhead -->
   <div class="main" id="main-content"> 
     <div class="container">
-      <h1>Data Reviews</h1>
+      <h1 style="font-size:35px;">Data Reviews</h1>
     </div>
     
     <br />
